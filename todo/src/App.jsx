a60df20a30bc9fa040b658ac65a793c32a10d8d6
risquaMussa <rismussa@gmail.com>
2024-06-19 12:46:41 -0400

    updated

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,12 +1,19 @@
-const todoList = [
-  { id: 1, title: "Complete assignemnt" },
-  { id: 2, title: "Buy groceries" },
-  { id: 3, title: "Call friend" },
-];
-<h1>Todo List</h1>;
+function App() {
+  const todoList = [
+    { id: 1, title: "Complete assignemnt" },
+    { id: 2, title: "Buy groceries" },
+    { id: 3, title: "Call friend" },
+  ];
 
-<ul>
-  {todoList.map((todo) => (
-    <li key={todo.id}>{todo.title}</li>
-  ))}
-</ul>;
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todoList.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default App;
