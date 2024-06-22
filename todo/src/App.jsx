ee0307daf296b5807ab3b7fe2667e0910e07a1bc
risquaMussa <rismussa@gmail.com>
2024-06-22 17:16:22 -0400

    Add a form with label, input, and submit button for todo entries

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,18 +1,11 @@
+import AddTodoForm from "./AddTodoForm";
+import TodoList from "./TodoList";
 function App() {
-  const todoList = [
-    { id: 1, title: "Complete assignemnt" },
-    { id: 2, title: "Buy groceries" },
-    { id: 3, title: "Call friend" },
-  ];
-
   return (
     <div>
       <h1>Todo List</h1>
-      <ul>
-        {todoList.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
-      </ul>
+      <AddTodoForm />
+      <TodoList />
     </div>
   );
 }
