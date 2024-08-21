0b659032aee27ec2ecd8d15cc6fd0b9bad28e23b
risquaMussa <rismussa@gmail.com>
2024-08-20 22:37:44 -0400

    Fetch Data from Airtable

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,40 @@ import { useEffect, useState } from "react";
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  //start from here
+  const fetchData = async () => {
+    const option = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`,
+      },
+    };
+    const URL = `https://api.airtable.com/v0/${
+      import.meta.env.VITE_AIRTABLE_BASE_ID
+    }/${import.meta.env.VITE_TABLE_NAME}`;
+
+    try {
+      const response = await fetch(URL, option);
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const data = await response.json();
+
+      const todos = data.records.map((record) => ({
+        title: record.fields.title,
+        id: record.id,
+      }));
+      setTodoList(todos);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Fetch error:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   // JSON.parse(localStorage.getItem("savedTodoList"));
   useEffect(() => {
     new Promise((resolve) => {
